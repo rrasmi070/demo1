82f9d8e070c46eae2f56f6b1a05d1afbf40512aa
rrasmi070 <67046373+rrasmi070@users.noreply.github.com>
2024-03-29 00:28:32 +0530

    1

--- demo1/urls.py
+++ demo1/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from demo1 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index, name='index'),
+
 ]
---
+++ demo1/views.py
@@ -0,0 +1,5 @@
+# myapp/views.py
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, World!")
